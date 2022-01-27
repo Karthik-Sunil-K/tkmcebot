64c6aea367603f9fe63a6c4a30d6894c00984cf4
e-labinnovations <mohammedashad23@gmail.com>
2022-01-27 23:30:33 +0530

    New featers added

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 /* TESTING START */
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 // const bot = new Telegraf('1129048108:AAG0hrQhTwqNHyed5159EyTpk0TeM4E9q0E');
-const bot = new Telegraf('927312041:AAHS_s1hYTrPup8zO4tyKaS-Vwh9x8Px_ok');
+// const bot = new Telegraf('927312041:AAHS_s1hYTrPup8zO4tyKaS-Vwh9x8Px_ok');
 /* TESTING END */
 
 /* PRODUCTION START */
-// const { Composer } = require('micro-bot');
-// const bot = new Composer;
+const { Composer } = require('micro-bot');
+const bot = new Composer;
 /* PRODUCTION END */
 
 const axios = require('axios');
-const Extra = require('telegraf/extra')
-const Markup = require('telegraf/markup')
 var { subjectsData, studyMaterials } = require('./data')
 var admins = [{ chatId: 625310795, userId: 'elab_innovations', name: 'e-lab innovations' }, { chatId: 591998055, userId: 'KarthikSunilK', name: 'Karthik Sunil K' }]
+var uploadMaterials = {}
 
-// const api = "https://script.google.com/a/tkmce.ac.in/macros/s/AKfycby_dH5yS_23YXDLGuAp-B0uH3H3UVYTW9onliiq5DLw9JB7y85hP8LjwCTL56kegkSxKA/exec"
-const api = "https://script.google.com/a/tkmce.ac.in/macros/s/AKfycbysbSOP4OO4XJ96jsp6hzIvDA7f06PrqORkLWR7X6iueHyhIC7TnSZm2vSacy6IWwTPSw/exec"
+const api = "https://script.google.com/a/tkmce.ac.in/macros/s/AKfycbzKZvQrIDbNmbLuGV6BPDy-AJnBMeC-yMwm-ZjUW9Bdo4WI_w-r-ZelG0K0DZ7qudUx3Q/exec"
 const updateData = (ctx) => {
     axios.get(api + '?action=getSubjects')
         .then(function(response) {
@@ -44,6 +42,20 @@ const updateData = (ctx) => {
             console.log(error);
             if (ctx) {
                 ctx.telegram.sendMessage(ctx.chat.id, 'studyMaterials updation failed, ' + studyMaterials.length, +' items')
+            }
+        });
+
+    axios.get(api + '?action=getAdmins')
+        .then(function(response) {
+            admins = response.data ? response.data : admins
+            if (ctx) {
+                ctx.telegram.sendMessage(ctx.chat.id, 'admins updated, ' + admins.length, +' items')
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+            if (ctx) {
+                ctx.telegram.sendMessage(ctx.chat.id, 'admins updation failed, ' + admins.length, +' items')
             }
         });
 }
@@ -131,7 +143,7 @@ const sendSubjectDetails = (code, ctx) => {
     const mateCount = module => {
         return studyMaterials.filter(material => {
             return material.subjectCode == subCode &&
-                (material.modules.includes(module + ''))
+                (material.modules.includes(module + '') || module == 'A')
         }).length
     }
 
@@ -141,7 +153,8 @@ const sendSubjectDetails = (code, ctx) => {
                 inline_keyboard: [
                     [{ text: `Module 1 📚 [${mateCount(1)}]`, callback_data: ['mod', subCode, '1', dept, sem].join('_') }, { text: `Module 2 📚 [${mateCount(2)}]`, callback_data: ['mod', subCode, '2', dept, sem].join('_') }],
                     [{ text: `Module 3 📚 [${mateCount(3)}]`, callback_data: ['mod', subCode, '3', dept, sem].join('_') }, { text: `Module 4 📚 [${mateCount(4)}]`, callback_data: ['mod', subCode, '4', dept, sem].join('_') }],
-                    [{ text: `Module 5 📚 [${mateCount(5)}]`, callback_data: ['mod', subCode, '5', dept, sem].join('_') }, { text: "◀️ Back", callback_data: [dept, sem].join('_') }]
+                    [{ text: `Module 5 📚 [${mateCount(5)}]`, callback_data: ['mod', subCode, '5', dept, sem].join('_') }, { text: `All 📚 [${mateCount('A')}]`, callback_data: ['mod', subCode, 'A', dept, sem].join('_') }],
+                    [{ text: "◀️ Back", callback_data: [dept, sem].join('_') }]
                 ]
             },
             parse_mode: "MarkdownV2"
@@ -162,7 +175,7 @@ const sendModuleDetails = (code, ctx) => {
     const mateCount = type => {
         return studyMaterials.filter(material => {
             return material.subjectCode == subCode &&
-                (material.modules.includes(module + '')) &&
+                (material.modules.includes(module + '') || module == 'A') &&
                 material.type == type
         }).length
     }
@@ -171,7 +184,7 @@ const sendModuleDetails = (code, ctx) => {
         ctx.editMessageText(`*${codeToName(dept)}* \\- ${sem}\n*${subCode}* \\- ${subject.name}\nMedule ${module}\nSelect your type`, {
             reply_markup: {
                 inline_keyboard: [
-                    [{ text: `🗒 Class Notes [${mateCount('CN')}]`, callback_data: ['mate_CN', subCode, module, dept, sem].join('_') }, { text: `📄 Printed Notes [${mateCount('PN')}]`, callback_data: ['mate_PN', subCode, module, dept, sem].join('_') }],
+                    [{ text: `✍️ Class Notes [${mateCount('CN')}]`, callback_data: ['mate_CN', subCode, module, dept, sem].join('_') }, { text: `📄 Printed Notes [${mateCount('PN')}]`, callback_data: ['mate_PN', subCode, module, dept, sem].join('_') }],
                     [{ text: `📚 Text Books [${mateCount('TB')}]`, callback_data: ['mate_TB', subCode, module, dept, sem].join('_') }, { text: `📃 Q Papers [${mateCount('QP')}]`, callback_data: ['mate_QP', subCode, module, dept, sem].join('_') }],
                     [{ text: `🎞 Videos [${mateCount('V')}]`, callback_data: ['mate_V', subCode, module, dept, sem].join('_') }, { text: "◀️ Back", callback_data: ['sub', subCode, dept, sem].join('_') }]
                 ]
@@ -188,7 +201,7 @@ const sendMeterialDetails = (code, ctx) => {
 
     let materials = studyMaterials.filter(material => {
         return material.subjectCode == subCode &&
-            material.modules.includes(module + '') &&
+            (material.modules.includes(module + '') || module == 'A') &&
             material.type == mateType
     })
 
@@ -209,18 +222,41 @@ const sendMeterialDetails = (code, ctx) => {
 
         if (material.type == 'V') {
             try {
-                await ctx.telegram.sendMessage(ctx.chat.id, `*${material.name}*\n${material.content}\n\n @tkmcebot`, Extra.markdown())
+                await ctx.replyWithMarkdown(`*${material.name}*\n${material.content}\n\n @tkmcebot`)
             } catch (error) {
-                reportError(`sendMessage - ${content}`, ctx)
+                console.log(error);
+                reportError(`sendMessage - ${material.content}`, ctx)
             }
         }
     })
 }
 
+const addMaterialToDB = (code, ctx) => {
+    let message_id = code.split('_')[1]
+    let uploadData = uploadMaterials[message_id]
+    if (uploadData) {
+        axios.get(`https://script.google.com/a/tkmce.ac.in/macros/s/AKfycbzKZvQrIDbNmbLuGV6BPDy-AJnBMeC-yMwm-ZjUW9Bdo4WI_w-r-ZelG0K0DZ7qudUx3Q/exec?action=addMaterial&subjectCode=${uploadData.subjectCode}&module=${uploadData.modules}&type=${uploadData.type}&name=${uploadData.name}&content=${uploadData.content}`)
+            .then(function(response) {
+                if (response.data.success) {
+                    ctx.reply(`New material added\n click /updateDB to update database`)
+                } else {
+                    ctx.reply(`Failed from server\n ${response.data.error}`)
+                }
+            })
+            .catch(function(error) {
+                console.log(error);
+                ctx.reply(`Error\n\n${JSON.stringify(error)}`)
+            }).then(function() {
+                delete uploadMaterials[message_id]
+                ctx.deleteMessage()
+            })
+    }
+}
+
 //Start
 bot.start((ctx) => {
 
-    axios.get(`https://script.google.com/a/tkmce.ac.in/macros/s/AKfycbysbSOP4OO4XJ96jsp6hzIvDA7f06PrqORkLWR7X6iueHyhIC7TnSZm2vSacy6IWwTPSw/exec?action=newUser&userId=${ctx.chat.username?ctx.chat.username:''}&chatId=${ctx.chat.id}&name=${ctx.message.chat.first_name} ${ctx.message.chat.last_name}`)
+    axios.get(`https://script.google.com/a/tkmce.ac.in/macros/s/AKfycbzKZvQrIDbNmbLuGV6BPDy-AJnBMeC-yMwm-ZjUW9Bdo4WI_w-r-ZelG0K0DZ7qudUx3Q/exec?action=newUser&userId=${ctx.chat.username?ctx.chat.username:''}&chatId=${ctx.chat.id}&name=${ctx.message.chat.first_name} ${ctx.message.chat.last_name}`)
         .then(function(response) {
             // console.log(response);
             let isNew = response.data.success
@@ -240,7 +276,7 @@ I'm tkmce bot`)
                         [{ text: "CIVIL", callback_data: "ce" }, { text: "MECHANICAL", callback_data: "mech" }],
                         [{ text: "ELECTRICAL & ELECTRONICS", callback_data: "eee" }, { text: "ELECTRONICS & COMMUNICATION", callback_data: "ece" }],
                         [{ text: "COMPUTER SCIENCE", callback_data: "cse" }, { text: "CHEMICAL", callback_data: "ce" }],
-                        [{ text: "ARCHITECTURE", callback_data: "archi" }, { text: "🧑‍💻 Contact Admin", callback_data: "contactAdmin" }]
+                        [{ text: "ARCHITECTURE", callback_data: "archi" }, { text: "⚙️ More Options", callback_data: "more-options" }]
                     ]
                 }
             })
@@ -255,7 +291,7 @@ bot.hears(['Hi', 'hi', 'Hii', 'hii', 'hai', 'Hai'], (ctx) => {
                 [{ text: "CIVIL", callback_data: "ce" }, { text: "MECHANICAL", callback_data: "mech" }],
                 [{ text: "ELECTRICAL & ELECTRONICS", callback_data: "eee" }, { text: "ELECTRONICS & COMMUNICATION", callback_data: "ece" }],
                 [{ text: "COMPUTER SCIENCE", callback_data: "cse" }, { text: "CHEMICAL", callback_data: "ce" }],
-                [{ text: "ARCHITECTURE", callback_data: "archi" }, { text: "🧑‍💻 Contact Admin", callback_data: "contactAdmin" }]
+                [{ text: "ARCHITECTURE", callback_data: "archi" }, { text: "⚙️ More Options", callback_data: "more-options" }]
             ]
         }
     })
@@ -269,22 +305,49 @@ bot.action('menu', (ctx) => {
                 [{ text: "CIVIL", callback_data: "ce" }, { text: "MECHANICAL", callback_data: "mech" }],
                 [{ text: "ELECTRICAL & ELECTRONICS", callback_data: "eee" }, { text: "ELECTRONICS & COMMUNICATION", callback_data: "ece" }],
                 [{ text: "COMPUTER SCIENCE", callback_data: "cse" }, { text: "CHEMICAL", callback_data: "ce" }],
-                [{ text: "ARCHITECTURE", callback_data: "archi" }, { text: "🧑‍💻 Contact Admin", callback_data: "contactAdmin" }]
+                [{ text: "ARCHITECTURE", callback_data: "archi" }, { text: "⚙️ More Options", callback_data: "more-options" }]
+            ]
+        }
+    })
+})
+
+//More options
+bot.action('more-options', ctx => {
+    ctx.editMessageText('More Options', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "🧑‍💻 Contact Developers", callback_data: "contactDevs" }],
+                [{ text: "Contribute", callback_data: "contribute" }],
+                [{ text: "◀️ Back", callback_data: "menu" }]
             ]
         }
     })
 })
 
 //Conact admin option
-bot.action('contactAdmin', ctx => {
-    ctx.editMessageText('Contact Admin', {
+bot.action('contactDevs', ctx => {
+    ctx.editMessageText('Contact Developers', {
         reply_markup: {
             inline_keyboard: [
                 [{ text: "Karthik", url: "https://wa.me/918606683287?text=Hello from tkmce bot" }],
                 [{ text: "e-lab innovations", url: "https://t.me/elab_innovations" }],
-                [{ text: "◀️ Back", callback_data: "menu" }]
+                [{ text: "◀️ Back", callback_data: "more-options" }]
             ]
         }
+    })
+})
+
+//Contriute
+bot.action('contribute', ctx => {
+            let msg = `I appreciate your interest in contributing.
+Contact <b>admins</b> to upload study materials.
+Thank you 😍
+
+<b>ADMINS</b>
+${admins.map(admin => `🧑‍💻 <a href="tg://user?id=${admin.chatId}">${admin.userId}</a>`).join('\n')}
+`
+    ctx.telegram.sendMessage(ctx.chat.id, msg, {
+        parse_mode: 'HTML'
     })
 })
 
@@ -319,6 +382,9 @@ bot.on('callback_query', (ctx) => {
     if (qData.startsWith('mate_')) {
         sendMeterialDetails(qData, ctx)
     }
+    if (qData.startsWith('addMaterial_')) {
+        addMaterialToDB(qData, ctx)
+    }
 
     // console.log(qData);
 
@@ -346,13 +412,87 @@ bot.command('updateDB', ctx => {
 bot.command('getid', ctx => {
     if (ctx.update.message.reply_to_message) {
         if (ctx.update.message.reply_to_message.document) {
-            ctx.reply("`" + ctx.update.message.reply_to_message.document.file_id + "`", Extra.markdownV2())
+            ctx.replyWithMarkdown("`" + ctx.update.message.reply_to_message.document.file_id + "`")
         } else {
             ctx.reply("That is not a document")
         }
     } else {
         ctx.reply("Send as a reply to a document")
     }
+})
+
+bot.command('addMaterial', (ctx) => {
+    let isAdmin = admins.find(admin => admin.chatId == ctx.chat.id)
+    if (isAdmin) {
+    let text = ctx.update.message.text
+    text = text.replace('/addMaterial ', '')
+    let parameters = text.split('&')
+    if (!(parameters.length == 4)) {
+        ctx.reply(`Send like
+/addMaterial subjectCode&moduels&type&name
+
+subjectCode: Subject in capital letters
+modules: Modules sperated by comma(,)
+type: Type of the material
+      CN - Class Note
+      PN - Printed Note
+      TB - Text Book
+      QP - Question Paper
+      V - Video link
+name: Name of the material
+
+eg: /addMaterial HUT200&1,2&CN&Module 2 Class Note`)
+    } else {
+        if (ctx.update.message.reply_to_message) {
+            let subjectCode = parameters[0]
+            let modules = parameters[1]
+            let type = parameters[2]
+            let name = parameters[3]
+
+            if (!(subjectsData.find(m => m.code == subjectCode))) {
+                ctx.reply("Subject code not found\nCheck subject code or update DB")
+            } else if(!(type == 'CN' || type == 'PN' || type == 'TB' || type == 'QP' || type == 'V')) {
+                ctx.reply("Wrong type")
+            } else if (ctx.update.message.reply_to_message.document) {
+                let content = ctx.update.message.reply_to_message.document.file_id;
+                uploadMaterials[ctx.update.message.message_id] = {subjectCode, modules, type, name, content}
+                ctx.reply(`Your study material is ready upload
+Name: ${name}
+Suject: ${subjectCode + ' - ' + subjectsData.find(m => m.code == subjectCode).name}
+Modules: ${modules}
+Type: ${type}
+Content: ${content}`, {
+    reply_markup: {
+        inline_keyboard: [
+            [{ text: "⬆️ Upload", callback_data: "addMaterial_" + ctx.update.message.message_id }]
+        ]
+    }
+})
+            } else {
+                let content = ctx.update.message.reply_to_message.text;
+                type = 'V'
+                uploadMaterials[ctx.update.message.message_id] = {subjectCode, modules, type, name, content}
+                ctx.reply(`Your study material is ready upload
+Name: ${name}
+Suject: ${subjectCode + ' - ' + subjectsData.find(m => m.code == subjectCode).name}
+Modules: ${modules}
+Type: ${type}
+Content: ${content}`, {
+    reply_markup: {
+        inline_keyboard: [
+            [{ text: "⬆️ Upload", callback_data: "addMaterial_" + ctx.update.message.message_id }]
+        ]
+    }
+})
+            }
+        } else {
+            ctx.reply("This command works only for reply.\nReply to a document or text")
+        }
+    }
+    
+} else {
+    ctx.telegram.sendMessage(ctx.chat.id, '❗️ You can\'t do that')
+}
 })
 
 
@@ -363,15 +503,16 @@ bot.on('document', (ctx) => {
         // console.log(ctx);
 })
 
+
 updateData();
 
 //shrouded-brushlands-98310
 //https://shrouded-brushlands-98310.herokuapp.com/
 
 /* PRODUCTION START */
-// module.exports = bot;
+module.exports = bot;
 /* PRODUCTION END */
 
 /* TEST START */
-bot.launch();
+// bot.launch();
 /* TEST END */
